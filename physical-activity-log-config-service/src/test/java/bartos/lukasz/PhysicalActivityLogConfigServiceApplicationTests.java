4e1fd5f910a63cf392753f6e3d70a5ee049c8582
root <lbertas1@wp.pl>
2021-01-15 23:09:46 +0100

    adding property file to test

---
+++ physical-activity-log-config-service/src/test/java/bartos/lukasz/PhysicalActivityLogConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bartos.lukasz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalActivityLogConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
