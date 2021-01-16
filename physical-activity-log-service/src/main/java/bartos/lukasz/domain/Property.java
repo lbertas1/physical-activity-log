b138772506ebe0357d2ee482adbff62c9fa646c5
root <lbertas1@wp.pl>
2021-01-16 12:46:30 +0100

    working on config service

--- physical-activity-log-service/src/main/java/bartos/lukasz/domain/Property.java
+++ physical-activity-log-service/src/main/java/bartos/lukasz/domain/Property.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("property")  // prefix zmiennych z pliku properties ktore beda tutaj zaciagane
+//@ConfigurationProperties("property")  // prefix zmiennych z pliku properties ktore beda tutaj zaciagane
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
